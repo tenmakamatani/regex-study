d3286711caac437bedd6cef608ca84e122f5744b
tenmakamatani <tenmakamatani0@gmail.com>
2019-08-20 23:09:22 +0900

    [update]デリミタを使用

--- regex.js
+++ regex.js
@@ -8,58 +8,58 @@ function test(regex, text) {
 
 // .
 // 全ての文字列にマッチ
-const regex1 = new RegExp(".");
+const regex1 = /./;
 
 // 文字列abcにマッチ
-const regex2 = new RegExp("abc");
+const regex2 = /abc/;
 
 // ^
 // 先頭がabcの文字列("abcde", "abc34234"など)にマッチ
-const regex3 = new RegExp("^abc");
+const regex3 = /^abc/;
 
 // $
 // 最後がabcの文字列("deabc", "34234abc"など)にマッチ
-const regex4 = new RegExp("abc$");
+const regex4 = /abc$/;
 
 // *
 // 直前の文字が0文字以上ある文字列("ac", "abbbbc"など)にマッチ
-const regex5 = new RegExp("ab*c");
+const regex5 = /ab*c/;
 
 // +
 // 直前の文字が1文字以上ある文字列("abc", "abbbc"など)にマッチ
-const regex6 = new RegExp("ab+c");
+const regex6 = /ab+c/;
 
 // ?
 // 直前の文字が0文字か1文字ある文字列("ac", "abc"のみ)にマッチ
-const regex7 = new RegExp("ab?c");
+const regex7 = /ab?c/;
 
 // |
 // 「|」で区切られた文字列のいずれかが存在する文字列("aaa1111", "222ddd22"など)にマッチ
-const regex8 = new RegExp("aaa|bbb|ccc|ddd");
+const regex8 = /aaa|bbb|ccc|ddd/;
 
 // []
 // []の中のどれかにマッチする文字列("abc", "aac"など)にマッチ
-const regex9 = new RegExp("a[abc]c");
+const regex9 = /a[abc]c/;
 // aからzのどれか
-const regex10 = new RegExp("a[a-z]c");
+const regex10 = /a[a-z]c/;
 // AからZ, aからz, 0から9のどれか
-const regex11 = new RegExp("a[A-Za-z0-9]c");
+const regex11 = /a[A-Za-z0-9]c/;
 
 // [^]
 // []の中のどれか以外の文字列にマッチ
-const regex12 = new RegExp("a[^bc]c");
+const regex12 = /a[^bc]c/;
 
 // ()
 // ()で囲まれた部分をグループ化する
 
 // "abc", "abababc"など
-const regex13 = new RegExp("(ab)+c");
+const regex13 = /(ab)+c/;
 // "aaac", "abbc"など
-const regex14 = new RegExp("a(aa|bb|cc)c");
+const regex14 = /a(aa|bb|cc)c/;
 
 // {}
 // {}内の数字だけ前の文字を繰り返ししている文字列にマッチ
-const regex15 = new RegExp("ab{3}c"); // 3回
-const regex16 = new RegExp("ab{1,3}c"); // 1回~3回
-const regex17 = new RegExp("ab{1,}c"); // 1回以上
-const regex18 = new RegExp("ab{,3}c"); // 3回以下
+const regex15 = /ab{3}c/; // 3回
+const regex16 = /ab{1,3}c/; // 1回~3回
+const regex17 = /ab{1,}c/; // 1回以上
+const regex18 = /ab{,3}c/; // 3回以下
